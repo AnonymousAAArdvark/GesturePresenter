f6042a9d2d76d03bbf5d0dc8002302e1333cfd07
AnonymousAAArdvark <andrewhuiyang0830@gmail.com>
2024-10-12 16:26:13 -0700

    import camera

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Camera from './Camera';
+
 
 const App: React.FC = () => {
   return (
     <div>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      <Camera flipped={false}/>
     </div>
   );
 };
