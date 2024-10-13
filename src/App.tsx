33424e8a5db1bcd9d2a16776678eb1eb4489ce84
AnonymousAAArdvark <andrewhuiyang0830@gmail.com>
2024-10-12 17:14:34 -0700

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,15 @@ import SettingsModal from "./SettingsModal";
 
 const App: React.FC = () => {
   const [flipped, setFlipped] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(true);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const hideModal = () => {
+    setIsModalVisible(false);
+  };
 
   const handlePairingCodeSubmit = (code: string) => {
     console.log("Pairing code submitted:", code);
@@ -17,10 +26,11 @@ const App: React.FC = () => {
   return (
     <div>
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-      <SettingsModal
+      {isModalVisible && <div className="modal-overlay"></div>}
+      {isModalVisible &&  <SettingsModal
         onPairingCodeSubmit={handlePairingCodeSubmit}
         onToggleGestureSwap={handleToggleGestureSwap}
-      />
+      />}
       <Camera flipped={flipped}/>
     </div>
   );
